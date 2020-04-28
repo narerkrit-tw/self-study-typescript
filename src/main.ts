6e99c02437274ada51174e7f1bc4b4e4bffc99b8
narerkrit sinrachtanant <narerkrit.s@thoughtworks.com>
2020-04-28 16:22:12 +0700

    added simple main file

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+type Point2D = {
+  x: number,
+  y: number
+}
+
+const Point2D = (x: number, y: number) => ({
+  x: x,
+  y: y
+})
+
+class Point2DImpl implements Point2D{
+  readonly x: number;
+  readonly y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+}
+
+
+const p1: Point2D = Point2D(10,0)
+
+const p2: Point2D = new Point2DImpl(20,15);
+
+console.log(p1);
+console.log(p2);
+
