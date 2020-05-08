e4c9d69e184a1b65501626885999098cc73f2160
thank narerkrit <narerkrit.s@thoughtworks.com>
2020-05-08 12:48:40 +0700

    make suit name a finite set of string

--- src/card/Suit.ts
+++ src/card/Suit.ts
@@ -2,9 +2,11 @@ import { Bounded } from "fp-ts/lib/Bounded"
 import { ord, ordNumber } from "fp-ts/lib/Ord"
 import { TinyType } from "tiny-types"
 
+type SuitName = "club" | "diamond" | "heart" | "spade"
+
 export default class Suit extends TinyType {
   private constructor(
-    public readonly name: string,
+    public readonly name: SuitName,
     public readonly order: number
   ) {
     super()
@@ -12,15 +14,31 @@ export default class Suit extends TinyType {
   diff(that: Suit): number {
     return this.order - that.order
   }
-  static Club = new Suit("Club", 1)
-  static Diamond = new Suit("Diamond", 2)
-  static Heart = new Suit("Heart", 3)
-  static Spade = new Suit("Spade", 4)
-  static All = [Suit.Club, Suit.Diamond, Suit.Heart, Suit.Spade]
+  static Club = new Suit("club", 1)
+  static Diamond = new Suit("diamond", 2)
+  static Heart = new Suit("heart", 3)
+  static Spade = new Suit("spade", 4)
+  static All = [
+    Suit.Club, 
+    Suit.Diamond, 
+    Suit.Heart, 
+    Suit.Spade
+  ]
 
   static IsBounded: Bounded<Suit> = {
     ... ord.contramap(ordNumber, (s: Suit) => s.order),
     top: Suit.Club,
     bottom: Suit.Spade,
+  }
+}
+
+
+function match(s: Suit): string {
+  switch (s.name) {
+    case "club": return  "it is a club"
+    case "diamond": return "tis a diamond"
+    case "heart": return "here lies a heart"
+    case "spade": return "look, a spade"
+    // case "other": return "this doesn't belong"
   }
 }
