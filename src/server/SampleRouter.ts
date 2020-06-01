c8dcb7ee0703f40c9b8c71682d2b21ad0dfbef75
thank narerkrit <narerkrit.s@thoughtworks.com>
2020-06-01 17:21:23 +0700

    try set response status with side-effectful method

--- src/server/SampleRouter.ts
+++ src/server/SampleRouter.ts
@@ -1,19 +1,25 @@
 import * as express from "express"
+import * as E from "fp-ts/lib/Either"
+import { pipe } from "fp-ts/lib/pipeable"
 import * as T from "io-ts"
 import { optionFromNullable as TOpt } from "io-ts-types/lib/optionFromNullable"
 import RestypedRouter from "restyped-express-async"
 
 const SampleReq = T.type({
-    someNumber: T.number,
-    optionalBoolean: TOpt(T.boolean)
+  someNumber: T.number,
+  optionalBoolean: TOpt(T.boolean)
 })
 
 type SampleReq = T.TypeOf<typeof SampleReq>
+type SampleResp = {
+  success: boolean
+}
 
 type SampleAPI = {
   "/sample": {
     POST: {
       body: SampleReq
+      response: SampleResp
     }
   }
 }
@@ -21,12 +27,16 @@ type SampleAPI = {
 const apiRouter = express.Router()
 const router = RestypedRouter<SampleAPI>(apiRouter)
 
-router.post("/sample", async req => {
+router.post("/sample", async (req, resp) => {
   const decodeResult = SampleReq.decode(req.body)
-  console.log("got sample request")
-  console.log(`${JSON.stringify(decodeResult)}`)
-  console.log(`${typeof decodeResult}`)
-  return decodeResult
+  pipe(
+    decodeResult,
+    E.fold(err => 400, body => 201 ),
+    x => resp.status(x)
+  )
+  return {
+    success: E.isRight(decodeResult)
+  }
 })
 
 export default apiRouter
